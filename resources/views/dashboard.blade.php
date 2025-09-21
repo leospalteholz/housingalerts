8398d97f23aa61fbcde3bf5870ad342742aaf904
Leo Spalteholz <leo.spalteholz@gmail.com>
2025-09-21 08:37:22 -0700

    Remove some unused files

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -77,10 +77,10 @@
                                                                 <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path>
                                                             </svg>
                                                             <span>
-                                                                @if($hearing->start_date)
-                                                                    {{ \Carbon\Carbon::parse($hearing->start_date)->format('M j, Y') }}
-                                                                    @if($hearing->start_time)
-                                                                        at {{ \Carbon\Carbon::parse($hearing->start_time)->format('g:i A') }}
+                                                                @if($hearing->start_datetime)
+                                                                    {{ \Carbon\Carbon::parse($hearing->start_datetime)->format('M j, Y') }}
+                                                                    @if($hearing->start_datetime)
+                                                                        at {{ \Carbon\Carbon::parse($hearing->start_datetime)->format('g:i A') }}
                                                                     @endif
                                                                 @else
                                                                     Date TBD
