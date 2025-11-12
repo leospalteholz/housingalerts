44acc3cfd7d7bfd567af8577e77e34533a532668
Leo Spalteholz <leo.spalteholz@gmail.com>
2025-11-11 20:47:32 -0800

    don't leak whether emails exist on password reset.

--- tests/Feature/Auth/PasswordResetTest.php
+++ tests/Feature/Auth/PasswordResetTest.php
@@ -25,8 +25,9 @@ class PasswordResetTest extends TestCase
 
         $user = User::factory()->create();
 
-        $this->post('/forgot-password', ['email' => $user->email]);
+    $response = $this->post('/forgot-password', ['email' => $user->email]);
 
+    $response->assertSessionHas('status', 'If an account exists for that email address, a password reset link has been sent.');
         Notification::assertSentTo($user, ResetPassword::class);
     }
 
